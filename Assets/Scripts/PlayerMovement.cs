f84128d4341f2010da6122ab7999f6d5b903ad7e
AmitAceAvr <amit.avrashmi@gmail.com>
2026-04-15 20:11:57 +0300

    playerMovement
    8 directional movement using WASD could also work with the arrows

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public float speed = 1f;
+    private Rigidbody2D rb;
+    private Vector2 input;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        input.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = input * speed;
+    }
+}
