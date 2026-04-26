f0235fad9ea76edd0f212038949c6b8a99f88950
TfehLsdw <danxdlel@gmail.com>
2026-04-26 12:07:46 +0300

    UnitBase migration start
    created a unitBase abstract class to migrate most functionality from unitStats  and many others into it, as well as allowing subclasses for each unit

--- Assets/Scripts/ScriptableObjectTypes/UnitStatProfile.cs
+++ Assets/Scripts/ScriptableObjectTypes/UnitStatProfile.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "UnitStatsProfile", menuName = "Units/Unit Stat Profile")]
 public class UnitStatProfile : ScriptableObject
 {
-    public float maxHP = 10;
-    public float speed = 2;
-    public float damage = 5;
+    public float maxHP = 10f;
+    public float speed = 2f;
+    public float damage = 5f;
+    public float attackCooldown = 0.35f;
+    public bool isFriendly = false;
 }
