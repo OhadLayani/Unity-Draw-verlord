fdf78a7e5f1c00ef7b4dd66ea9ea1212f4102de8
OhadLayani <103877865+OhadLayani@users.noreply.github.com>
2026-06-11 23:39:29 +0300

    Revert "UI adjustment"

--- Assets/Scripts/UI/InkCounterUI.cs
+++ Assets/Scripts/UI/InkCounterUI.cs
@@ -1,22 +1,16 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class InkCounterUI : MonoBehaviour
 {
     [SerializeField] private PlayerController player;
-   // [SerializeField] private TMP_Text inkText;
-    [SerializeField] private Image _image;
-    private int maxValue;
-    private int currentValue;
-    private float targetFill;
-    [SerializeField] int LerpSpeed;
+    [SerializeField] private TMP_Text inkText;
+
     private void Update()
     {
-        maxValue = player.maxInkCount;
-        currentValue = player.InkCount;
-        targetFill = Mathf.Clamp01((float)currentValue / maxValue);
+        if (player == null || inkText == null)
+            return;
 
-         _image.fillAmount = Mathf.Lerp(_image.fillAmount, targetFill, LerpSpeed * Time.deltaTime);
+        inkText.text = $"{player.InkCount} / {player.maxInkCount}";
     }
 }
