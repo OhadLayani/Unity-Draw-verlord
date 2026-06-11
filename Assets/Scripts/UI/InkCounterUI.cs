f572ee514361c6668dc6036ba04a3faa148b36ba
AmitAceAvr <amit.avrashmi@gmail.com>
2026-06-11 13:26:32 +0300

    UI adjustment
    new doodle + ink counter, pause menu, controls panel inside the pause menu

--- Assets/Scripts/UI/InkCounterUI.cs
+++ Assets/Scripts/UI/InkCounterUI.cs
@@ -1,16 +1,22 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InkCounterUI : MonoBehaviour
 {
     [SerializeField] private PlayerController player;
-    [SerializeField] private TMP_Text inkText;
-
+   // [SerializeField] private TMP_Text inkText;
+    [SerializeField] private Image _image;
+    private int maxValue;
+    private int currentValue;
+    private float targetFill;
+    [SerializeField] int LerpSpeed;
     private void Update()
     {
-        if (player == null || inkText == null)
-            return;
+        maxValue = player.maxInkCount;
+        currentValue = player.InkCount;
+        targetFill = Mathf.Clamp01((float)currentValue / maxValue);
 
-        inkText.text = $"{player.InkCount} / {player.maxInkCount}";
+         _image.fillAmount = Mathf.Lerp(_image.fillAmount, targetFill, LerpSpeed * Time.deltaTime);
     }
 }
